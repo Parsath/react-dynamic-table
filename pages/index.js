6f0e0e44455ef54aa0f670cae7e8ff0f012d96ff
Salah Azzouz <salah.azzouz@breakpoint-technology.fr>
2021-10-09 20:23:11 +0100

    style and update index.js component

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head';
-import { StyledButton } from '../src/Button.styled';
+import Button from '../src/Button.styled';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Button Modifier</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        {/* <StyledButton className="rounded-full text-purple-600 border border-purple-200 px-9 py-6 bg-purple-200"> */}
-        <StyledButton $color="yellow" rounded="true">
-          GO
-        </StyledButton>
-      </main>
-
-
-
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">This is a button demo</span>
+          <span className="block">Using Tailwind Css and styled component</span>
+        </h2>
+        <div className="mt-8 flex justify-center">
+          <div className="inline-flex rounded-md shadow"></div>
+          <div className="ml-3 inline-flex">
+            {/* Buttons Section */}
+            <Button $primary={true}>Primary</Button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
