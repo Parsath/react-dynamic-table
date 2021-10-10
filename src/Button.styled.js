3eba2504bf4051e9da2624987d5548891e6eb833
Salah Azzouz <salah.azzouz@breakpoint-technology.fr>
2021-10-10 06:48:02 +0100

    create generic buttons

--- src/Button.styled.js
+++ src/Button.styled.js
@@ -1,7 +1,38 @@
 import tw from 'tailwind-styled-components';
 
-const Button = tw.div`
-    ${(p) => (p.$primary ? 'bg-indigo-600' : 'bg-indigo-300')}
+const getClassColor = (color) => {
+  let bucket;
+  switch (color) {
+    case 'blue':
+      bucket = 'bg-blue-400';
+      break;
+
+    case 'green':
+      bucket = 'bg-green-400';
+      break;
+
+    case 'red':
+      bucket = 'bg-red-400';
+      break;
+
+    case 'yellow':
+      bucket = 'bg-yellow-400';
+      break;
+
+    default:
+      bucket = 'bg-gray-400';
+      break;
+  }
+  return bucket;
+};
+
+const getHoverClass = (color) => `hover:bg-${color}-500`;
+const getFocusClass = (color) => `focus:ring-${color}-500`;
+
+const Button = tw.button`
+    ${(p) => getClassColor(p.$color)}
+    ${(p) => getHoverClass(p.$color)}
+    ${(p) => getFocusClass(p.$color)}
       inline-flex 
       items-center 
       px-3.5 
@@ -13,12 +44,9 @@ const Button = tw.div`
       rounded 
       shadow-sm 
       text-white 
-      bg-indigo-600 
-      hover:bg-indigo-700 
       focus:outline-none 
       focus:ring-2 
       focus:ring-offset-2 
-      focus:ring-indigo-500
 `;
 
 export default Button;
